3908a16669106b636325fbfcfa596e9f0adcd233
anusha MONDRU <anushamo.in@mouritech.com>
2021-11-19 12:12:18 +0000

    first commit

---
+++ jsproject/reuse.js
@@ -0,0 +1,12 @@
+function onMagic(){
+	//step 1: get all the elements with class name box-title ---> array 
+	var aBoxTitle = document.getElementsByClassName("box-title");
+	//step 2: loop over the array elements (ui) --> inside each loop pass, we will process
+	for(var i=0; i<aBoxTitle.length; i++){
+		//Step 3: each loop pass, we get access to individual elements one by one
+		var oTitleElement = aBoxTitle[i];
+		oTitleElement.style.backgroundColor = "black";
+		oTitleElement.style.color = "white";
+	}
+	
+}
